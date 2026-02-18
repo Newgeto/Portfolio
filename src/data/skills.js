bfdca64418e8e7f8b9924699b2b707730d89879c
Yanou <yanis.mdoughy@outlook.fr>
2026-02-18 23:11:43 +0100

    update

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,30 +1,26 @@
-const CDN = 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons'
-const SIMPLE = 'https://cdn.simpleicons.org'
-
 export const SKILLS = {
   creation: [
-    { name: 'Canva', logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQi4yHiBqzGRehgq1MwiJ7yxP1Lzif73hTUw&s' },
-    { name: 'Blender', logo: `${CDN}/blender/blender-original.svg` },
-    { name: 'WordPress', logo: `${CDN}/wordpress/wordpress-original.svg` },
-    { name: 'Photoshop', logo: `${CDN}/photoshop/photoshop-plain.svg` },
-    { name: 'Figma', logo: `${CDN}/figma/figma-original.svg` },
+    { name: 'Canva', logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQi4yHiBqzGRehgq1MwiJ7yxP1Lzif73hTUw&s" },
+    { name: 'Blender', logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Blender_logo_no_text.svg/960px-Blender_logo_no_text.svg.png" },
+    { name: 'WordPress', logo: "https://s.w.org/style/images/about/WordPress-logotype-alternative.png" },
+    { name: 'Photoshop', logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/960px-Adobe_Photoshop_CC_icon.svg.png" },
+    { name: 'Figma', logo: "https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" },
   ],
   dev: [
-    { name: 'HTML', logo: `${CDN}/html5/html5-original.svg` },
-    { name: 'CSS', logo: `${CDN}/css3/css3-original.svg` },
-    { name: 'React', logo: `${CDN}/react/react-original.svg` },
-    { name: 'Express', logo: `${CDN}/express/express-original.svg` },
+    { name: 'HTML', logo: "https://img.icons8.com/?size=100&id=20909&format=png&color=000000" },
+    { name: 'CSS', logo: "https://img.icons8.com/?size=100&id=21278&format=png&color=000000" },
+    { name: 'React', logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/960px-React-icon.svg.png" },
     { name: 'Node.js', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1280px-Node.js_logo.svg.png' },
-    { name: 'JavaScript', logo: `${CDN}/javascript/javascript-original.svg` },
-    { name: 'TypeScript', logo: `${CDN}/typescript/typescript-original.svg` },
-    { name: 'Python', logo: `${CDN}/python/python-original.svg` },
-    { name: 'PHP', logo: `${CDN}/php/php-original.svg` },
+    { name: 'JavaScript', logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/960px-Unofficial_JavaScript_logo_2.svg.png" },
+    { name: 'TypeScript', logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" },
+    { name: 'Python', logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/3840px-Python-logo-notext.svg.png" },
+    { name: 'PHP', logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/3840px-PHP-logo.svg.png" },
   ],
   tools: [
-    { name: 'Git', logo: `${CDN}/git/git-original.svg` },
-    { name: 'GitHub', logo: `${CDN}/github/github-original.svg` },
-    { name: 'Docker', logo: `${CDN}/docker/docker-original.svg` },
-    { name: 'Burp Suite', logo: `${SIMPLE}/owasp/000000` },
-    { name: 'Postman', logo: `${CDN}/postman/postman-original.svg` },
+    { name: 'Git', logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/250px-Git_icon.svg.png" },
+    { name: 'GitHub', logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/langfr-250px-GitHub_Invertocat_Logo.svg.png" },
+    { name: 'Docker', logo: "https://icon-icons.com/download-file?file=https%3A%2F%2Fimages.icon-icons.com%2F2415%2FPNG%2F512%2Fdocker_original_logo_icon_146556.png&id=146556&pack_or_individual=pack" },
+    { name: 'Burp Suite', logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/BurpSuite_Comunity_Edition.svg/250px-BurpSuite_Comunity_Edition.svg.png" },
+    { name: 'Postman', logo: "https://play-lh.googleusercontent.com/MET411zZK3fH8Z4YjQR9iBrKEaqQKL-5UwOk2f0QGA2rp6L14AeQSDsvQR8wS1t8aJpulXrCXrtzTh3MsZ6VXeE" },
   ],
 }
