e120454fa87e3b545475adfe9147669c465539b1
Yanou <yanis.mdoughy@outlook.fr>
2026-02-18 21:29:13 +0100

    Fix About

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -4,7 +4,7 @@ function About() {
       <div className="sectionInner">
         <h2 className="sectionTitle">À propos</h2>
         <div className="aboutCopy">
-          <p>Je suis étudiant en informatique, curieux et motivé, avec une vraie envie d’apprendre et de progresser chaque jour. Je m’intéresse particulièrement au développement web et au développement de jeux vidéo. J’aime comprendre comment les choses fonctionnent, tester, créer et m’améliorer à travers des projets concrets.</p>
+          <p>Je suis étudiant en informatique, curieux et motivé, avec une vraie envie d’apprendre et de progresser chaque jour. Je m’intéresse particulièrement au développement web et de jeux vidéo. J’aime comprendre comment les choses fonctionnent, tester, créer et m’améliorer à travers des projets concrets.</p>
         </div>
       </div>
     </section>
